b93ac07fe50c0e6ba31b998d6c7feedc6171a040
ouahdinaaaa <a.ouahdina@gmail.com>
2023-12-17 19:53:21 +0100

    pushhhh

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayael-ou <ayael-ou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/17 15:18:25 by ayael-ou          #+#    #+#             */
+/*   Updated: 2023/12/17 19:45:28 by ayael-ou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <vector>
+#include <iostream>
+
+template<typename T>
+T easyfind(T &a, int nb)
+{}
+
+#endif

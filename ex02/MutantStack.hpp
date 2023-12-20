f4aed021b9da181f11a88c72bedf74c14e5a1b82
ouahdinaaaa <a.ouahdina@gmail.com>
2023-12-20 21:08:15 +0100

    push

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayael-ou <ayael-ou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/20 20:25:19 by ayael-ou          #+#    #+#             */
+/*   Updated: 2023/12/20 20:34:26 by ayael-ou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <string>
+#include <iostream>
+#include <stack>
+#include <list>
+
+class MutantStack
+{
+    private:
+    
+    
+    public:
+    
+    MutantStack();
+    ~MutantStack();
+};
+
+#endif
